8fa68fc56d20dda0210b6270c33b3a19801ad72c
Eskelevir <vitaliyantonoff@gmail.com>
2019-03-07 16:26:43 +0600

    Added error handling requestsю

--- xkcd.py
+++ xkcd.py
@@ -1,22 +1,53 @@
+import sys
 from random import randint
 
 import requests
+from requests.exceptions import ConnectionError, HTTPError
+
+from logging import config, getLogger
+
+config.fileConfig(fname="logger.cfg", disable_existing_loggers=False)
+logger = getLogger(__file__)
 
 
 def fetch_random_comic_json_url(url_last_comic):
-    response = requests.get(url_last_comic)
-    if response.ok:
+    try:
+        response = requests.get(url_last_comic)
+        response.raise_for_status()
+    except ConnectionError:
+        logger.critical("It seems the xkcd.com is not available")
+        sys.exit(1)
+    except HTTPError as error:
+        logger.error(f"HTTP error occurred: {error}")
+        sys.exit(1)
+    else:
         count_of_comics = response.json()["num"]
         return f"http://xkcd.com/{randint(1, count_of_comics)}/info.0.json"
 
 
 def fetch_comic_image_url(comic_url):
-    response = requests.get(comic_url)
-    if response.ok:
+    # TODO поменять комментарии
+    try:
+        response = requests.get(comic_url)
+    except ConnectionError:
+        logger.critical("It seems the xkcd.com is not available")
+        sys.exit(1)
+    except HTTPError as error:
+        logger.error(f"HTTP error occurred: {error}")
+        sys.exit(1)
+    else:
         return response.json()["img"]
 
 
 def fetch_author_comment(comic_url):
-    response = requests.get(comic_url)
-    if response.ok:
+    # TODO поменять комментарии
+    try:
+        response = requests.get(comic_url)
+    except ConnectionError:
+        logger.critical("It seems the xkcd.com is not available")
+        sys.exit(1)
+    except HTTPError as error:
+        logger.error(f"HTTP error occurred: {error}")
+        sys.exit(1)
+    else:
         return response.json()["alt"]
